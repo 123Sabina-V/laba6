aec48f14877755ae3316ac8e08570a3babf2bdfd
123Sabina-V <56916317+123Sabina-V@users.noreply.github.com>
2019-11-06 11:54:41 +0300

    v.1.0

---
+++ lr 5/lr 5.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main()
+
+{
+	int a = 1;
+	int b = 3;
+	double h = 0.2;
+	float y, x = a;
+	while (x <= b)
+	{
+		y = tan(pow(x, 3)) + 2 * sin(x) - 3;
+		cout << x << endl;
+		cout << y << endl;
+		x += h;
+	}
+
+	
+	return 0;
+	system("pause");
+}
